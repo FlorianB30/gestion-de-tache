83e808319526f1d92afcdeadeecf6430657b1e59
FlorianB30 <96230889+FlorianB30@users.noreply.github.com>
2024-11-26 11:19:14 +0100

    first commit

---
+++ task-manager/src/app/components/task-box/task-box.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-box.component.html',
+  styleUrl: './task-box.component.scss'
+})
+export class TaskBoxComponent {
+
+}
---
+++ task-manager/src/app/components/task-line/task-line.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-line',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-line.component.html',
+  styleUrl: './task-line.component.scss'
+})
+export class TaskLineComponent {
+
+}
