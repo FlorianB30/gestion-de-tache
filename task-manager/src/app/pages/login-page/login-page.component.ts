192fcaabc66b5293949eb235dc630a578e4f7719
FlorianB30 <96230889+FlorianB30@users.noreply.github.com>
2024-11-26 11:48:38 +0100

    log and resgiter forms

--- task-manager/src/app/pages/login-page/login-page.component.ts
+++ task-manager/src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  logUser() {
+    if (this.loginForm.valid) {
+      console.log('Form Submitted', this.loginForm.value);
+    }
+  }
 }
--- task-manager/src/app/pages/register-page/register-page.component.ts
+++ task-manager/src/app/pages/register-page/register-page.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss'
 })
 export class RegisterPageComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  registerUser() {
+    if (this.registerForm.valid) {
+      console.log('Form Submitted', this.registerForm.value);
+    }
+  }
 }
